b106d6543e389e9ee923839b620e841c35b0875d
a797405 <arek90urban@gmail.com>
2024-01-29 16:37:57 +0100

    add Chrome driver

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("flag 01")
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+# driver = webdriver.Edge()
+driver = webdriver.Chrome()
+
+driver.get("https://fantasy.ekstraklasa.org/player/1221")
+
+name = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/section/div/div[2]/div/h1").text
+price = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div/table/tbody/tr[1]/td[2]").text
+
+print(f"name: {name}, price: {price}")
