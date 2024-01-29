0e49019886295745f25936fd7605251a9793c5c8
a797405 <arek90urban@gmail.com>
2024-01-29 16:38:58 +0100

    add Chrome driver

--- main.py
+++ main.py
@@ -13,4 +13,5 @@ driver.get("https://fantasy.ekstraklasa.org/player/1221")
 name = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/section/div/div[2]/div/h1").text
 price = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div/table/tbody/tr[1]/td[2]").text
 
+
 print(f"name: {name}, price: {price}")
