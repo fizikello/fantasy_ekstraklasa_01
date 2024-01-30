a393173175020810b4c69b19eb591eeefffe9a5e
a797405 <arek90urban@gmail.com>
2024-01-30 08:59:38 +0100

    use Secrets

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from secrets import Secrets
 
+s = Secrets()
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 # driver = webdriver.Edge()
 driver = webdriver.Chrome()
 
-driver.get("https://fantasy.ekstraklasa.org/player/1221")
+driver.get(s.url)
 
 name = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/section/div/div[2]/div/h1").text
 price = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div/table/tbody/tr[1]/td[2]").text
