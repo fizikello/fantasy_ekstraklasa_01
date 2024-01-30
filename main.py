d066b4236dfaffa126726a28e005d5c36fa95e7a
a797405 <arek90urban@gmail.com>
2024-01-30 10:17:37 +0100

    add marks

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from secrets import Secrets
+from marks import Marks
 
 s = Secrets()
+m = Marks()
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 # driver = webdriver.Edge()
 driver = webdriver.Chrome()
 
 driver.get(s.url)
+scratched_values = list()
 
-name = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/section/div/div[2]/div/h1").text
-price = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div/table/tbody/tr[1]/td[2]").text
+for mark in m.marks.values():
+    scratched_values.append(driver.find_element(By.XPATH, mark).text)
 
 
-print(f"name: {name}, price: {price}")
+
+print(f"{scratched_values}")
---
+++ marks.py
@@ -0,0 +1,7 @@
+class Marks:
+
+    def __init__(self):
+
+        self.marks = {"name": "/html/body/div[1]/div[2]/section/div/div[2]/div/h1",
+                "price": "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div/table/tbody/tr[1]/td[2]"}
+
