229db4efaf48bcfc042a1cb2428e4319cb7a4020
a797405 <arek90urban@gmail.com>
2024-01-30 12:59:06 +0100

    add pandas

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import time
 
+import pandas as pd
+import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from secrets import Secrets
+from selenium.common.exceptions import NoSuchElementException
+from hidden_values import Secrets
 from marks import Marks
+
 
 s = Secrets()
 m = Marks()
@@ -15,9 +19,28 @@ driver = webdriver.Chrome()
 driver.get(s.url)
 scratched_values = list()
 
-for mark in m.marks.values():
-    scratched_values.append(driver.find_element(By.XPATH, mark).text)
+for key, mark in m.marks.items():
+    try:
+        scratched_values.append(driver.find_element(By.XPATH, mark).text)
+    except NoSuchElementException:
+        print(f"Not found {key} markup")
 
+try:
+    table_xpath = driver.find_element(By.XPATH, m.table)
+    rows = table_xpath.find_elements(By.TAG_NAME, "tr")
 
+    data = []
+    col_data = rows[0].find_elements(By.TAG_NAME, "td")
+    column_names = [col.text for col in col_data]
+    column_names[-3] = "Yellow_Cards"
+    column_names[-2] = "Red_Cards"
 
-print(f"{scratched_values}")
+    for row in rows[1:]:
+        cols = row.find_elements(By.TAG_NAME, "td")
+        row_data = [col.text for col in cols]
+        data.append(row_data)
+except NoSuchElementException:
+    print("table not found")
+
+df = pd.DataFrame(data, columns=column_names)
+print(df)
--- marks.py
+++ marks.py
@@ -3,5 +3,14 @@ class Marks:
     def __init__(self):
 
         self.marks = {"name": "/html/body/div[1]/div[2]/section/div/div[2]/div/h1",
-                "price": "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div/table/tbody/tr[1]/td[2]"}
+                "price": "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div/table/tbody/tr[1]/td[2]",
+                "club_position": "/html/body/div[1]/div[2]/section/div/div[2]/div/div/span",
+                "popularity": "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div/table/tbody/tr[2]/td[2]",
+                "country" : "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div/table/tbody/tr[3]/td[2]",
+                "previous_club" : "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div/table/tbody/tr[4]/td[2]",
+                "sum_points" : "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[2]/div/div/div[2]/strong",
+                "sum_goals": "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[3]/div/div/div[2]/strong",
+                "sum_assists" : "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[4]/div/div/div[2]/strong"
+        }
 
+        self.table = "/html/body/div[1]/div[2]/div/div[2]/div/div/div/div/table"
